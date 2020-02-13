082d76e560782fe1b114818bb5c480bd8caae6f9
voxel-tracer <voxel.tracer@gmail.com>
2020-02-12 20:23:50 -0800

    faster way to initialize curand

--- kernels.cu
+++ kernels.cu
@@ -82,7 +82,7 @@ __global__ void render_init(int max_x, int max_y, curandState* rand_state) {
     if ((i >= max_x) || (j >= max_y)) return;
     int pixel_index = j * max_x + i;
     //Each thread gets same seed, a different sequence number, no offset
-    curand_init(1984, pixel_index, 0, &rand_state[pixel_index]);
+    curand_init(1984 + pixel_index, 0, 0, &rand_state[pixel_index]);
 }
 
 // Matching the C++ code would recurse enough into color() calls that
